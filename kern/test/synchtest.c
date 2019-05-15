59780b9c8654a1945f72d5edda5eaebe2c875978
ATEZ <shieldnet64@gmail.com>
2019-05-15 23:52:30 +0900

    Change Line position of gostraight(), has exception

--- kern/test/synchtest.c
+++ kern/test/synchtest.c
@@ -81,175 +81,6 @@ message(int msg_nr, int carnumber, int cardirection, int destdirection)
 
 static
 void
-inititems(void)
-{
-	if (testsem==NULL) {
-		testsem = sem_create("testsem", 2);
-		if (testsem == NULL) {
-			panic("synchtest: sem_create failed\n");
-		}
-	}
-	if (testlock==NULL) {
-		testlock = lock_create("testlock");
-		if (testlock == NULL) {
-			panic("synchtest: lock_create failed\n");
-		}
-	}
-	if (testcv==NULL) {
-		testcv = cv_create("testlock");
-		if (testcv == NULL) {
-			panic("synchtest: cv_create failed\n");
-		}
-	}
-	if (donesem==NULL) {
-		donesem = sem_create("donesem", 0);
-		if (donesem == NULL) {
-			panic("synchtest: sem_create failed\n");
-		}
-	}
-}
-
-static
-void
-semtestthread(void *junk, unsigned long num)
-{
-	int i;
-	(void)junk;
-
-	/*
-	 * Only one of these should print at a time.
-	 */
-	P(testsem);
-	kprintf("Thread %2lu: ", num);
-	for (i=0; i<NSEMLOOPS; i++) {
-		kprintf("%c", (int)num+64);
-	}
-	kprintf("\n");
-	V(donesem);
-}
-
-int
-semtest(int nargs, char **args)
-{
-	int i, result;
-
-	(void)nargs;
-	(void)args;
-
-	inititems();
-	kprintf("Starting semaphore test...\n");
-	kprintf("If this hangs, it's broken: ");
-	P(testsem);
-	P(testsem);
-	kprintf("ok\n");
-
-	for (i=0; i<NTHREADS; i++) {
-		result = thread_fork("semtest", NULL, semtestthread, NULL, i);
-		if (result) {
-			panic("semtest: thread_fork failed: %s\n",
-			      strerror(result));
-		}
-	}
-
-	for (i=0; i<NTHREADS; i++) {
-		V(testsem);
-		P(donesem);
-	}
-
-	/* so we can run it again */
-	V(testsem);
-	V(testsem);
-
-	kprintf("Semaphore test done.\n");
-	return 0;
-}
-
-static
-void
-fail(unsigned long num, const char *msg)
-{
-	kprintf("thread %lu: Mismatch on %s\n", num, msg);
-	kprintf("Test failed\n");
-
-	lock_release(testlock);
-
-	V(donesem);
-	thread_exit();
-}
-
-static
-void
-locktestthread(void *junk, unsigned long num)
-{
-	int i;
-	(void)junk;
-
-	for (i=0; i<NLOCKLOOPS; i++) {
-		lock_acquire(testlock);
-		testval1 = num;
-		testval2 = num*num;
-		testval3 = num%3;
-
-		if (testval2 != testval1*testval1) {
-			fail(num, "testval2/testval1");
-		}
-
-		if (testval2%3 != (testval3*testval3)%3) {
-			fail(num, "testval2/testval3");
-		}
-
-		if (testval3 != testval1%3) {
-			fail(num, "testval3/testval1");
-		}
-
-		if (testval1 != num) {
-			fail(num, "testval1/num");
-		}
-
-		if (testval2 != num*num) {
-			fail(num, "testval2/num");
-		}
-
-		if (testval3 != num%3) {
-			fail(num, "testval3/num");
-		}
-
-		lock_release(testlock);
-	}
-	V(donesem);
-}
-
-
-int
-locktest(int nargs, char **args)
-{
-	int i, result;
-
-	(void)nargs;
-	(void)args;
-
-	inititems();
-	kprintf("Starting lock test...\n");
-
-	for (i=0; i<NTHREADS; i++) {
-		result = thread_fork("synchtest", NULL, locktestthread,
-				     NULL, i);
-		if (result) {
-			panic("locktest: thread_fork failed: %s\n",
-			      strerror(result));
-		}
-	}
-	for (i=0; i<NTHREADS; i++) {
-		P(donesem);
-	}
-
-	kprintf("Lock test done.\n");
-
-	return 0;
-}
-
-static
-void
 gostraight(unsigned long cardirection, unsigned long carnumber) {
   if (cardirection==0) {
     int i = 0;
@@ -367,6 +198,176 @@ gostraight(unsigned long cardirection, unsigned long carnumber) {
 	}
      
 }
+
+static
+void
+inititems(void)
+{
+	if (testsem==NULL) {
+		testsem = sem_create("testsem", 2);
+		if (testsem == NULL) {
+			panic("synchtest: sem_create failed\n");
+		}
+	}
+	if (testlock==NULL) {
+		testlock = lock_create("testlock");
+		if (testlock == NULL) {
+			panic("synchtest: lock_create failed\n");
+		}
+	}
+	if (testcv==NULL) {
+		testcv = cv_create("testlock");
+		if (testcv == NULL) {
+			panic("synchtest: cv_create failed\n");
+		}
+	}
+	if (donesem==NULL) {
+		donesem = sem_create("donesem", 0);
+		if (donesem == NULL) {
+			panic("synchtest: sem_create failed\n");
+		}
+	}
+}
+
+static
+void
+semtestthread(void *junk, unsigned long num)
+{
+	int i;
+	(void)junk;
+
+	/*
+	 * Only one of these should print at a time.
+	 */
+	P(testsem);
+	kprintf("Thread %2lu: ", num);
+	for (i=0; i<NSEMLOOPS; i++) {
+		kprintf("%c", (int)num+64);
+	}
+	kprintf("\n");
+	V(donesem);
+}
+
+int
+semtest(int nargs, char **args)
+{
+	int i, result;
+
+	(void)nargs;
+	(void)args;
+
+	inititems();
+	kprintf("Starting semaphore test...\n");
+	kprintf("If this hangs, it's broken: ");
+	P(testsem);
+	P(testsem);
+	kprintf("ok\n");
+
+	for (i=0; i<NTHREADS; i++) {
+		result = thread_fork("semtest", NULL, semtestthread, NULL, i);
+		if (result) {
+			panic("semtest: thread_fork failed: %s\n",
+			      strerror(result));
+		}
+	}
+
+	for (i=0; i<NTHREADS; i++) {
+		V(testsem);
+		P(donesem);
+	}
+	gostraight(0,0);
+	/* so we can run it again */
+	V(testsem);
+	V(testsem);
+
+	kprintf("Semaphore test done.\n");
+	return 0;
+}
+
+static
+void
+fail(unsigned long num, const char *msg)
+{
+	kprintf("thread %lu: Mismatch on %s\n", num, msg);
+	kprintf("Test failed\n");
+
+	lock_release(testlock);
+
+	V(donesem);
+	thread_exit();
+}
+
+static
+void
+locktestthread(void *junk, unsigned long num)
+{
+	int i;
+	(void)junk;
+
+	for (i=0; i<NLOCKLOOPS; i++) {
+		lock_acquire(testlock);
+		testval1 = num;
+		testval2 = num*num;
+		testval3 = num%3;
+
+		if (testval2 != testval1*testval1) {
+			fail(num, "testval2/testval1");
+		}
+
+		if (testval2%3 != (testval3*testval3)%3) {
+			fail(num, "testval2/testval3");
+		}
+
+		if (testval3 != testval1%3) {
+			fail(num, "testval3/testval1");
+		}
+
+		if (testval1 != num) {
+			fail(num, "testval1/num");
+		}
+
+		if (testval2 != num*num) {
+			fail(num, "testval2/num");
+		}
+
+		if (testval3 != num%3) {
+			fail(num, "testval3/num");
+		}
+
+		lock_release(testlock);
+	}
+	V(donesem);
+}
+
+
+int
+locktest(int nargs, char **args)
+{
+	int i, result;
+
+	(void)nargs;
+	(void)args;
+
+	inititems();
+	kprintf("Starting lock test...\n");
+
+	for (i=0; i<NTHREADS; i++) {
+		result = thread_fork("synchtest", NULL, locktestthread,
+				     NULL, i);
+		if (result) {
+			panic("locktest: thread_fork failed: %s\n",
+			      strerror(result));
+		}
+	}
+	for (i=0; i<NTHREADS; i++) {
+		P(donesem);
+	}
+
+	kprintf("Lock test done.\n");
+
+	return 0;
+}
+
 
 static
 void
